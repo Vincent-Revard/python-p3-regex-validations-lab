6ebda8b181c8d750d072e9306c028727e7cf42f8
Vincent Revard <Vincent.Revard@gmail.com>
2024-03-13 17:26:26 -0700

    Complete Lab

--- lib/regex.py
+++ lib/regex.py
@@ -4,10 +4,13 @@ import re
 # Feel free to encourage students to find oversights and add tests to this lab!
 
 name = r""
-name_regex = re.compile(name)
+name_regex = re.compile(r"[A-Z]{1}[a-z]*[\s\'][A-Z]{1}[a-z]+(-[A-Z]{1}[a-z]+)*")
+# Allow spaces, single quotes, and hyphens
+
 
 phone_number = r""
-phone_regex = re.compile(phone_number)
+phone_regex = re.compile(r"^(\(\d{3}\)\s?|\d{3}[-.\s]?)?\d{3}[-.\s]?\d{4}$")
+
 
 email_address = r""
-email_regex = re.compile(email_address)
+email_regex = re.compile(r"^[A-z._%+-][A-z0-9._%+-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")
